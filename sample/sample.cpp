0a023cb572acc39ff965afc2ae95f95dc016c294
Kohe Tokoi <tokoi@sys.wakayama-u.ac.jp>
2017-03-08 10:55:35 +0900

    座標返還のクラス Matrix を追加した

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -4,6 +4,238 @@
 #include "stdafx.h"
 #pragma comment(lib, "opengl32.lib")
 
+//
+// 変換行列
+//
+class Matrix
+{
+  // 変換行列の要素
+  GLfloat matrix[16];
+
+public:
+
+  // コンストラクタ
+  Matrix() {}
+
+  // 配列の内容で初期化するコンストラクタ
+  //   a: GLfloat 型の 16 要素の配列
+  Matrix(const GLfloat *a)
+  {
+    std::copy(a, a + 16, matrix);
+  }
+
+  // 変換行列の配列を返す
+  const GLfloat *get() const
+  {
+    return matrix;
+  }
+
+  // 乗算
+  //   m: Matrix 型の乗数
+  Matrix operator*(const Matrix &m) const
+  {
+    Matrix t;
+
+    for (int i = 0; i < 16; ++i)
+    {
+      const int j(i & 3), k(i & ~3);
+
+      t.matrix[i] =
+        matrix[0 + j] * m.matrix[k + 0] +
+        matrix[4 + j] * m.matrix[k + 1] +
+        matrix[8 + j] * m.matrix[k + 2] +
+        matrix[12 + j] * m.matrix[k + 3];
+    }
+
+    return t;
+  }
+
+  // 単位行列を作成する
+  static Matrix identity()
+  {
+    Matrix t;
+
+    std::fill(t.matrix, t.matrix + 16, 0.0f);
+    t.matrix[0] = t.matrix[5] = t.matrix[10] = t.matrix[15] = 1.0f;
+
+    return t;
+  }
+
+  // (x, y, z) だけ平行移動する変換行列を作成する
+  static Matrix translate(GLfloat x, GLfloat y, GLfloat z)
+  {
+    Matrix t(identity());
+
+    t.matrix[12] = x;
+    t.matrix[13] = y;
+    t.matrix[14] = z;
+
+    return t;
+  }
+
+  // (x, y, z) 倍に拡大縮小する変換行列を作成する
+  static Matrix scale(GLfloat x, GLfloat y, GLfloat z)
+  {
+    Matrix t(identity());
+
+    t.matrix[0] = x;
+    t.matrix[5] = y;
+    t.matrix[10] = z;
+
+    return t;
+  }
+
+  // (x, y, z) を軸に a 回転する変換行列を作成する
+  static Matrix rotate(GLfloat a, GLfloat x, GLfloat y, GLfloat z)
+  {
+    Matrix t(identity());
+    const GLfloat d(sqrt(x * x + y * y + z * z));
+
+    if (d > 0.0f)
+    {
+      const GLfloat l(x / d), m(y / d), n(z / d);
+      const GLfloat l2(l * l), m2(m * m), n2(n * n);
+      const GLfloat lm(l * m), mn(m * n), nl(n * l);
+      const GLfloat c(cos(a)), c1(1.0f - c), s(sin(a));
+
+      t.matrix[0] = (1.0f - l2) * c + l2;
+      t.matrix[1] = lm * c1 + n * s;
+      t.matrix[2] = nl * c1 - m * s;
+
+      t.matrix[4] = lm * c1 - n * s;
+      t.matrix[5] = (1.0f - m2) * c + m2;
+      t.matrix[6] = mn * c1 + l * s;
+
+      t.matrix[8] = nl * c1 + m * s;
+      t.matrix[9] = mn * c1 - l * s;
+      t.matrix[10] = (1.0f - n2) * c + n2;
+    }
+
+    return t;
+  }
+
+  // ビュー変換行列を作成する
+  static Matrix lookat(
+    GLfloat ex, GLfloat ey, GLfloat ez,   // 視点の位置
+    GLfloat gx, GLfloat gy, GLfloat gz,   // 目標点の位置
+    GLfloat ux, GLfloat uy, GLfloat uz)   // 上方向のベクトル
+  {
+    // 平行移動の変換行列
+    const Matrix tv(translate(-ex, -ey, -ez));
+
+    // t 軸 = e - g
+    const GLfloat tx(ex - gx);
+    const GLfloat ty(ey - gy);
+    const GLfloat tz(ez - gz);
+
+    // r 軸 = u x t 軸
+    const GLfloat rx(uy * tz - uz * ty);
+    const GLfloat ry(uz * tx - ux * tz);
+    const GLfloat rz(ux * ty - uy * tx);
+
+    // s 軸 = t 軸 x r 軸
+    const GLfloat sx(ty * rz - tz * ry);
+    const GLfloat sy(tz * rx - tx * rz);
+    const GLfloat sz(tx * ry - ty * rx);
+
+    // s 軸の長さのチェック
+    const GLfloat s2(sx * sx + sy * sy + sz * sz);
+    if (s2 == 0.0f) return tv;
+
+    // 回転の変換行列
+    Matrix rv(identity());
+
+    // r 軸を正規化して配列変数に格納
+    const GLfloat r(sqrt(rx * rx + ry * ry + rz * rz));
+    rv.matrix[0] = rx / r;
+    rv.matrix[4] = ry / r;
+    rv.matrix[8] = rz / r;
+
+    // s 軸を正規化して配列変数に格納
+    const GLfloat s(sqrt(s2));
+    rv.matrix[1] = sx / s;
+    rv.matrix[5] = sy / s;
+    rv.matrix[9] = sz / s;
+
+    // t 軸を正規化して配列変数に格納
+    const GLfloat t(sqrt(tx * tx + ty * ty + tz * tz));
+    rv.matrix[2] = tx / t;
+    rv.matrix[6] = ty / t;
+    rv.matrix[10] = tz / t;
+
+    // 視点の平行移動の変換行列に視線の回転の変換行列を乗じる
+    return rv * tv;
+  }
+
+  // 直交投影変換行列を作成する
+  static Matrix orthogonal(GLfloat left, GLfloat right,
+    GLfloat bottom, GLfloat top,
+    GLfloat zNear, GLfloat zFar)
+  {
+    Matrix t(identity());
+    const GLfloat dx(right - left);
+    const GLfloat dy(top - bottom);
+    const GLfloat dz(zFar - zNear);
+
+    if (dx != 0.0f && dy != 0.0f && dz != 0.0f)
+    {
+      t.matrix[0] = 2.0f / dx;
+      t.matrix[5] = 2.0f / dy;
+      t.matrix[10] = -2.0f / dz;
+      t.matrix[12] = -(right + left) / dx;
+      t.matrix[13] = -(top + bottom) / dy;
+      t.matrix[14] = -(zFar + zNear) / dz;
+    }
+
+    return t;
+  }
+
+  // 透視投影変換行列を作成する
+  static Matrix frustum(GLfloat left, GLfloat right,
+    GLfloat bottom, GLfloat top,
+    GLfloat zNear, GLfloat zFar)
+  {
+    Matrix t(identity());
+    const GLfloat dx(right - left);
+    const GLfloat dy(top - bottom);
+    const GLfloat dz(zFar - zNear);
+
+    if (dx != 0.0f && dy != 0.0f && dz != 0.0f)
+    {
+      t.matrix[0] = 2.0f * zNear / dx;
+      t.matrix[5] = 2.0f * zNear / dy;
+      t.matrix[8] = (right + left) / dx;
+      t.matrix[9] = (top + bottom) / dy;
+      t.matrix[10] = -(zFar + zNear) / dz;
+      t.matrix[11] = -1.0f;
+      t.matrix[14] = -2.0f * zFar * zNear / dz;
+      t.matrix[15] = 0.0f;
+    }
+
+    return t;
+  }
+
+  // 画角を指定して透視投影変換行列を作成する
+  static Matrix perspective(GLfloat fovy, GLfloat aspect,
+    GLfloat zNear, GLfloat zFar)
+  {
+    Matrix t(identity());
+    const GLfloat dz(zFar - zNear);
+
+    if (dz != 0.0f)
+    {
+      t.matrix[5] = 1.0f / tan(fovy * 0.5f);
+      t.matrix[0] = t.matrix[5] / aspect;
+      t.matrix[10] = -(zFar + zNear) / dz;
+      t.matrix[11] = -1.0f;
+      t.matrix[14] = -2.0f * zFar * zNear / dz;
+      t.matrix[15] = 0.0f;
+    }
+
+    return t;
+  }
+};
+
 /*
 ** シェーダオブジェクトのコンパイル結果を表示する
 */
@@ -256,16 +488,10 @@ int _tmain(int argc, _TCHAR* argv[])
 
     // 変換行列を作成する
     const GLfloat r(static_cast<GLfloat>(frame++) * 0.001f);
-    const GLfloat m[] =
-    {
-      cos(r), -sin(r), 0.0, 0.0,
-      sin(r), cos(r), 0.0, 0.0,
-      0.0, 0.0, 1.0, 0.0,
-      0.0, 0.0, 0.0, 1.0
-    };
+    Matrix m(Matrix::rotate(r, 0.0f, 0.0f, 1.0f));
 
     // uniform 変数に値を設定する
-    glUniformMatrix4fv(mLoc, 1, GL_FALSE, m);
+    glUniformMatrix4fv(mLoc, 1, GL_FALSE, m.get());
 
     // 図形を描画する
     glBindVertexArray(vao);
--- sample/simple.vert
+++ sample/simple.vert
@@ -1,12 +1,12 @@
 #version 330
 
 // 頂点データ
-in vec2 pv;
+in vec4 pv;
 
 // 変換行列
 uniform mat4 m;
 
 void main()
 {
-  gl_Position = m * vec4(pv, 0.0, 1.0);
+  gl_Position = m * pv;
 }
